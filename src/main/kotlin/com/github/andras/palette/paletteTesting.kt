6ee18fcd79c6b059e7acf6429860005fa8bd8ba8
Andrew <a.ferenczi.andras@gmail.com>
2018-11-30 09:02:10 +0000

    Palette is ready and sorted.

--- src/main/kotlin/com/github/andras/palette/paletteTesting.kt
+++ src/main/kotlin/com/github/andras/palette/paletteTesting.kt
@@ -1,23 +1,27 @@
 package com.github.andras.palette
 
 import com.github.ajalt.colormath.ConvertibleColor
+import com.github.ajalt.colormath.Mixer
 import com.github.ajalt.colormath.RGB
-import kotlinx.html.body
-import kotlinx.html.div
-import kotlinx.html.html
+import kotlinx.html.*
 import kotlinx.html.stream.appendHTML
-import kotlinx.html.style
 import java.io.File
 
 
-data class Palette(val colors: List<RGB>)
+data class Palette(
+        val name: String,
+        val colors: List<RGB>
+)
 
 fun createPalettePreview(palettes: List<Palette>): String {
-    val columnCount = palettes.getOrNull(0)?.colors?.size ?: 0
+    val colorCount = palettes.getOrNull(0)?.colors?.size ?: 0
+    val columnCount = (colorCount) + 1
 
-    require(palettes.all { it.colors.size == columnCount })
+    require(palettes.all { it.colors.size == colorCount })
 
     val columnNames = buildString { repeat(columnCount) { this.append(" auto") }; this.append(";") }
+
+    val elemHeight = 100
 
     return buildString {
         this.appendHTML()
@@ -27,9 +31,15 @@ fun createPalettePreview(palettes: List<Palette>): String {
                             style = "display: grid; grid-template-columns: $columnNames"
 
                             for (palette in palettes) {
+
+                                div {
+                                    style = "height: ${elemHeight}px;}"
+                                    +palette.name
+                                }
+
                                 for (color in palette.colors) {
                                     div {
-                                        style = "height: 200px; " +
+                                        style = "height: ${elemHeight}px; " +
                                                 "background-color: ${color.toHex(withNumberSign = true)}"
                                     }
                                 }
@@ -40,24 +50,71 @@ fun createPalettePreview(palettes: List<Palette>): String {
     }
 }
 
-fun <C : ConvertibleColor> createColorPalette(dark: RGB, middle: RGB, light: RGB) {
-    TODO()
+fun <C : ConvertibleColor> createColorPalette(
+        dark: RGB,
+        middle: RGB,
+        light: RGB,
+        convert: RGB.() -> C
+): Palette {
+
+    val half = 0.5
+
+    fun mixHalf(from: RGB, to: RGB) = Mixer.mix(from.convert(), to, half)
+
+    val c100 = light
+    val c500 = middle
+    val c900 = dark
+
+    val c300 = mixHalf(c100, c500)
+
+    val c400 = mixHalf(c300, c500)
+    val c200 = mixHalf(c100, c300)
+
+    val c700 = mixHalf(c500, c900)
+
+    val c600 = mixHalf(c500, c700)
+    val c800 = mixHalf(c700, c900)
+
+    return Palette(
+            dark.convert()::class.java.simpleName,
+            listOf(
+                    c100, c200, c300, c400, c500, c600, c700, c800, c900
+            )
+    )
 }
 
 fun main(args: Array<String>) {
 
-    val columnCount = 3
+    val c100 = RGB("#ffcc00")
+    val c500 = RGB("#00ccff")
+    val c900 = RGB("#004422")
 
-    val columnNames = buildString { repeat(columnCount) { this.append(" auto") }; this.append(";") }
+    val blendings = listOf<(RGB) -> ConvertibleColor>(
+            { it.toCMYK() },
+            { it.toRGB() },
+            { it.toLAB() },
+            { it.toXYZ() },
+            { it.toHSV() },
+            { it.toHSL() }
+    )
+
+    val palettes = blendings.map {
+        createColorPalette(
+                c100,
+                c500,
+                c900,
+                it
+        )
+    }
+
+    val html = createPalettePreview(palettes)
 
     val file = File("palette_preview.html")
 
+    println("Html content is:\n$html\n")
 
-
-//    println("Html content is:\n$html\n")
-//
-//    file.writeText(html)
-//    println("File written to ${file.absolutePath}")
+    file.writeText(html)
+    println("File written to ${file.absolutePath}")
 
 
 }
